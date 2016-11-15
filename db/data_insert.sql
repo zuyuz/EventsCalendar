5d30dd92766a4ecdd1dedf457e72e6b6607ac9c4
Admin <tmikent@gmail.com>
2016-11-16 01:32:08 +0200

    added sql code to fill db with data

---
+++ db/data_insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO "EventsScheduler"
+("Name", "Login", "Password", "UserRole")
+VALUES ('Admin', 'admin', 'admin', 0);
+
+INSERT INTO "EventsScheduler"
+("Name", "Login", "Password", "UserRole")
+VALUES ('Guest', 'guest', 'guest', 2);
+
+INSERT INTO "EventsScheduler"
+("Name", "Login", "Password", "UserRole")
+VALUES ('User', 'user', 'user', 1);
