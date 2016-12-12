dca160d1304f45efe5a6415001f11a3f6792c620
Admin <tmikent@gmail.com>
2016-12-12 21:10:41 +0200

    added tests for sign in, sign out methods

--- EventsScheduler/EventsSchedulerTests/ControllerTests.cs
+++ EventsScheduler/EventsSchedulerTests/ControllerTests.cs
@@ -40,12 +40,57 @@ namespace EventsScheduler.Tests
                 .Returns(new User()
                 {
                     Login = "admin",
-                    Password = "admin"
+                    Password = "pass"
                 });
-            var task = controller.SignInAsync("admin", "admin");
+            var task = controller.SignInAsync("admin", "pass");
             task.Wait();
 
             Assert.IsTrue(task.Result);
+            Assert.AreEqual("admin", controller.CurrentUser.Login);
+            Assert.AreEqual("pass", controller.CurrentUser.Password);
+        }
+
+        [TestMethod()]
+        public void SignInAsyncIdentificationFailTest()
+        {
+            users.Setup(_ => _.GetUserByLogin("none"))
+                .Returns<User>(null);
+            var task = controller.SignInAsync("none", "_");
+            task.Wait();
+
+            Assert.IsFalse(task.Result);
+        }
+
+        [TestMethod()]
+        public void SignInAsyncAuthentificationFailTest()
+        {
+            users.Setup(_ => _.GetUserByLogin("admin"))
+                .Returns(new User()
+                {
+                    Login = "admin",
+                    Password = "admin"
+                });
+            var task = controller.SignInAsync("admin", "bad_pass");
+            task.Wait();
+
+            Assert.IsFalse(task.Result);
+        }
+
+        [TestMethod()]
+        public void SignOutTest()
+        {
+            users.Setup(_ => _.GetUserByLogin("admin"))
+               .Returns(new User()
+               {
+                   Login = "admin",
+                   Password = "pass"
+               });
+            var task = controller.SignInAsync("admin", "pass");
+            task.Wait();
+            controller.SignOut();
+
+            Assert.IsTrue(task.Result);
+            Assert.IsNull(controller.CurrentUser);
         }
     }
 }
